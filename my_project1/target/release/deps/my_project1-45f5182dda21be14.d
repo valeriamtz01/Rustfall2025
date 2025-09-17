be34e27235e45ad66c663e9caccbb1869c740ffc
Valeria Martinez <valeria.martinez23@utrgv.edu>
2025-09-17 10:38:07 -0500

    Delete my_project1 directory

--- my_project1/target/release/deps/my_project1-45f5182dda21be14.d
+++
@@ -1,5 +0,0 @@
-/workspaces/Rustfall2025/my_project1/target/release/deps/my_project1-45f5182dda21be14.d: src/main.rs
-
-/workspaces/Rustfall2025/my_project1/target/release/deps/my_project1-45f5182dda21be14: src/main.rs
-
-src/main.rs:
